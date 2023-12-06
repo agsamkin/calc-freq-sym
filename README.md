6ab31fe0e0699377360467044fde48974d49d663
ags <a.g.samkin@gmail.com>
2023-12-06 12:28:17 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Calculation of the frequency of symbols
---
+++ src/test/java/home/ags/CalcFreqSymApplicationTests.java
@@ -0,0 +1,13 @@
+package home.ags;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalcFreqSymApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
